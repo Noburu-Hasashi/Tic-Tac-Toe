7647aaac2da3fe7590f243bd16d2354866d77782
Uday Angrish <blahblahuday@gmail.com>
2021-09-05 10:50:58 +0530

    - Added function get_move() to main.py

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ def render(board):  # gives an output of the board on the terminal
         print("|", board[row][2], "|")
         print(divider_string)
     print()
+
+def get_move():  # asks user to input co-ordinates and returns them
+    coordinates = (None, None)
+    x_coordinate = input("Enter the value for X co-ordinate: ")
+    y_coordinate = input("Enter the value for Y co-ordinate: ")
+    coordinates[0], coordinates[1] = x_coordinate, y_coordinate
+    return coordinates
