f55842f8946cc8b3f5609c987fdfe30fe19e33b3
Uday Angrish <blahblahuday@gmail.com>
2021-09-19 20:45:19 +0530

    - Edited get_move() in game.py - Edited render() in game.py - Corrected main.py into a working Tic-Tac-Toe game

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from game import *
 import random
 
-while True:
-    board = new_board()  # constructs an empty board
-    players = ['X', 'O']  # choses a random player to start the game
-    player = players[random.randint(0, 1)]
-    print("Player '", player, "' goes first!")
+board = new_board()  # constructs an empty board
+players = ['X', 'O']  # choses a random player to start the game
+current_player = players[random.randint(0, 1)]
+print("Player '", current_player, "' goes first!")
+render(board)
+for turn in range(9):  # makes the game run for 9 turns
+    print("ENTER COORDINATES FOR PLAYER ", current_player, ":")
+    coordinates = get_move()
+    make_move(coordinates, board, current_player)
     render(board)
-    for turn in range(9):  # makes the game run for 9 turns
-        coordinates = get_move()
-        make_move(coordinates, board, player)
-        winner = get_winner(board)
-        if winner == None:
-            if turn == 8:
-                print("- - -GAME ENDED! THIS GAME IS A DRAW!- - -")
-                break
-            if player == 'X':
-                player = 'O'
-            if player == 'O':
-                player = 'X'
-    break
+    winner = get_winner(board)
+    if winner == None:
+        if turn == 8:
+            print("- - -GAME ENDED! THIS GAME IS A DRAW!- - -")
+            break
+    if current_player == 'X':
+        current_player = 'O'
+    elif current_player == 'O':
+        current_player = 'X'
+    if winner == 'X':
+        print("- - -GAME ENDED! PLAYER 'X' IS THE WINNER!- - -")
+        break
+    if winner == 'O':
+        print("- - -GAME ENDED! PLAYER 'O' IS THE WINNER!- - -")
+        break
