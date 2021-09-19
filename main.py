fc28fde261a7b89815134a11c0713a6cc364e2bb
Uday Angrish <blahblahuday@gmail.com>
2021-09-19 10:50:53 +0530

    - Edited get_move() in game.py - Added loops in main.py for Tic-Tac-Toe game

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from game import *
+import random
+
+while True:
+    board = new_board()  # constructs an empty board
+    players = ['X', 'O']  # choses a random player to start the game
+    player = players[random.randint(0, 1)]
+    print("Player '", player, "' goes first!")
+    render(board)
+    for turn in range(9):  # makes the game run for 9 turns
+        coordinates = get_move()
+        make_move(coordinates, board, player)
+        winner = get_winner(board)
+        if winner == None:
+            if turn == 8:
+                print("- - -GAME ENDED! THIS GAME IS A DRAW!- - -")
+                break
+            if player == 'X':
+                player = 'O'
+            if player == 'O':
+                player = 'X'
+    break
