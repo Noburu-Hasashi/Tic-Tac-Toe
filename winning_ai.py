c6e660d40196b35a2a6317e6a20f4025ace9bbda
Uday Angrish <blahblahuday@gmail.com>
2021-09-22 19:38:00 +0530

    - Added function copy_board() to winning_ai.py - Added function winning_ai() to winning_ai.py

---
+++ winning_ai.py
@@ -0,0 +1,26 @@
+from game import *
+
+# makes a copy of the given board
+def copy_board(board):
+    copy_board = new_board()
+    for row in range(3):
+        for column in range(3):
+            board[row][column] = copy_board[row][column]
+    return copy_board
+
+# finds a winning move for current player, if not found, it returns a random move
+def finds_winning_moves_ai(board, player):
+    coordinates = [None, None]
+    temporary_board = copy_board(board)
+    for row in range(3):
+        for column in range(3):
+            if temporary_board[row][column] == None:
+                temporary_board[row][column] = player
+                if get_winner(temporary_board) == player:
+                    coordinates = [row, column]
+                    return coordinates
+                else:
+                    temporary_board[row][column] == None
+    # below code runs if no winner is found by adding current player using the above method
+    # returns None if no winning move is found
+    return None
