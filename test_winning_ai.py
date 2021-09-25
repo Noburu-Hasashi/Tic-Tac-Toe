fec8c227cd94928aba9e9719a018218870c99691
Uday Angrish <blahblahuday@gmail.com>
2021-09-25 08:51:16 +0530

    - Added more tests for test_find_winning_moves_ai() in test_winning_ai.py - Fixed error in winning_ai.py

--- test_winning_ai.py
+++ test_winning_ai.py
@@ -1,9 +1,33 @@
 from winning_ai import *
 
 def test_finds_winning_moves_ai():
-    sample_board = [
-        ['X', 'O', None],
-        [None, 'O', None],
-        ['X', None, None]
-    ]
-    assert finds_winning_moves_ai(sample_board, 'X') == [1,0]
+	sample_board = [
+		['X', 'O', None],
+		[None, 'O', None],
+		['X', None, None]
+	]
+	assert finds_winning_moves_ai(sample_board, 'X') == [1,0]
+	sample_board = [
+		['O', None, None],
+		['O', None, None],
+		[None, None, None]
+	]
+	assert finds_winning_moves_ai(sample_board, 'O') == [2,0]
+	sample_board = [
+		['O', 'O', None],
+		['X', None, None],
+		[None, None, None]
+	]
+	assert finds_winning_moves_ai(sample_board, 'O') == [0,2]
+	sample_board = [
+		['X', None, 'O'],
+		[None, 'O', None],
+		['X', None, None]
+	]
+	assert finds_winning_moves_ai(sample_board, 'X') == [1,0]
+	sample_board = [
+		['O',None, 'X'],
+		[None, 'X', None],
+		['O', None, None]
+	]
+	assert finds_winning_moves_ai(sample_board, 'O') == [1,0]
