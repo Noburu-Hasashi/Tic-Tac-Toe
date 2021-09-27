7cd9b8eb6c0157c0d1988ba290c1748b12739072
Uday Angrish <blahblahuday@gmail.com>
2021-09-27 09:19:25 +0530

    - Fixed function get_input_type() in main.py - Added function get_move_from_input_type() in main.py - Fixed main.py to run as a working Tic-Tac-toe game

--- main.py
+++ main.py
@@ -1,47 +1,78 @@
+from winning_ai import finds_winning_moves_ai
+from random_ai import random_ai
 from game import *
+from random_ai import *
 import random
 
 def get_input_type():
 	while True:
 		try:
-			player_1_input = int(input('''Enter the number for the type of input for player 1 :
+			player_X_input = int(input('''Enter the number for the type of input for player 'X' :
 	1. Asks user for input
 	2. Random AI
-	3. Winning AI'''))
-			if len(str(player_1_input)) != 1:
+	3. Winning AI
+	: '''))
+			if len(str(player_X_input)) != 1:
 				raise ValueError
-			player_2_input = int(input('''Enter the number for the type of input for player 2 :
+			player_O_input = int(input('''Enter the number for the type of input for player 'O' :
 	1. Asks user for input
 	2. Random AI
-	3. Winning AI'''))
-			if len(str(player_2_input)) != 1:
+	3. Winning AI
+	: '''))
+			if len(str(player_O_input)) != 1:
 				raise ValueError
 		except(ValueError, TypeError, SyntaxError):
 			print("- - -ERROR! INVALID INPUT! TRY AGAIN!- - -")
-	return player_1_input, player_2_input
+		finally:
+			return player_X_input, player_O_input
+
+def get_move_from_input_type(input_type_number, board, player):
+	if input_type_number == 1:
+		return get_move()
+	if input_type_number == 2:
+		return random_ai(board, player)
+	if input_type_number == 3:
+		coordinates = finds_winning_moves_ai(board, player)
+		if coordinates == None:
+			if player == 'X':
+				coordinates = finds_winning_moves_ai(board, 'O')
+				if coordinates == None:
+					coordinates = random_ai(board, 'X')
+			if player == 'O':
+				coordinates = finds_winning_moves_ai(board, 'X')
+				if coordinates == None:
+					coordinates = random_ai(board, 'O')
+		return coordinates
 
 board = new_board()  # constructs an empty board
+
+player_X_input, player_O_input = get_input_type()
 players = ['X', 'O']  # choses a random player to start the game
 current_player = players[random.randint(0, 1)]
+
 print("Player '", current_player, "' goes first!")
 render(board)
-for turn in range(9):  # makes the game run for 9 turns
-    print("ENTER COORDINATES FOR PLAYER ", current_player, ":")
-    coordinates = get_move()
-    make_move(coordinates, board, current_player)
-    render(board)
-    winner = get_winner(board)
-    if winner == None:
-        if turn == 8:
-            print("- - -GAME ENDED! THIS GAME IS A DRAW!- - -")
-            break
-    if current_player == 'X':
-        current_player = 'O'
-    elif current_player == 'O':
-        current_player = 'X'
-    if winner == 'X':
-        print("- - -GAME ENDED! PLAYER 'X' IS THE WINNER!- - -")
-        break
-    if winner == 'O':
-        print("- - -GAME ENDED! PLAYER 'O' IS THE WINNER!- - -")
-        break
+
+for turn in range(9):
+	if current_player == 'X':
+		current_move_type_number = player_X_input
+	elif current_player == 'O':
+		current_move_type_number = player_O_input
+	coordinates = get_move_from_input_type(current_move_type_number, board, current_player)
+	make_move(coordinates, board, current_player)
+	render(board)
+	winner = get_winner(board)
+	if winner == None:
+		if turn == 8:
+			print("- - -GAME ENDED! THIS GAME IS A DRAW!- - -")
+			break
+	if current_player == 'X':
+		current_player = 'O'
+	elif current_player == 'O':
+		current_player = 'X'
+	if winner == 'X':
+		print("- - -GAME ENDED! PLAYER 'X' IS THE WINNER!- - -")
+		break
+	if winner == 'O':
+		print("- - -GAME ENDED! PLAYER 'O' IS THE WINNER!- - -")
+		break
