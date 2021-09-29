5bdf47f1d5d5e76d11acbf0233fa2bc69defd75c
Uday Angrish <blahblahuday@gmail.com>
2021-09-29 18:02:45 +0530

    - Added function get_legal_moves() in minimax_ai.py

--- minimax_ai.py
+++ minimax_ai.py
@@ -1,5 +1,13 @@
 from game import *
 
+def get_legal_moves(board):
+    moves = []
+    for row in range(3):
+        for column in range(3):
+            if board[row][column] == None:
+                moves.append([row, column])
+    return moves
+
 def minimax_score(board, current_player):
     if get_winner(board) == current_player:
         return +10
@@ -8,5 +16,3 @@ def minimax_score(board, current_player):
             return 0
         else:
             return -10
-
-    
